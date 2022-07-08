8c321583fc8d08d122e82f7c2df8a7a533188cd0
lefepa <lfpatino82@misena.edu.co>
2022-07-08 10:05:57 -0500

    arreglo p

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <div>
           <h1>Mi primera aplicación</h1>
-          <p>Esta es mi primera aplicación en React, está padrísimo</p>
+          <p>Esta es mi primera aplicación en React, está padrísimo!</p>
         </div>
       </header>
     </div>
